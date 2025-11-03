332aff4370525ab47415eb6a000677ee565d9fc6
rafsan1p <rubayetrafsan605@gmail.com>
2025-11-04 04:49:24 +0600

    deploy seetup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/B12-A08-Hero-IO-Apps/',
   build: {
     rollupOptions: {
       output: {
